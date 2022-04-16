d285f1e0d17f2822bddf78c407763a37f751e66d
MerryDev <svgr1808@outlook.de>
2022-04-16 12:40:09 +0200

    Initial commit

---
+++ src/main/java/de/digitaldevs/flashblock/gamestate/LobbyState.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022. DigitalDevs
+ */
+
+package de.digitaldevs.flashblock.gamestate;
+
+import de.digitaldevs.flashblock.FlashBlockPlugin;
+import de.digitaldevs.gameapi.gamestate.GameState;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LobbyState implements GameState {
+
+    private final FlashBlockPlugin plugin;
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
