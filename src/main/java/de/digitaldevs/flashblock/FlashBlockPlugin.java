8896a9b7bd5d733cf23ba77da0050b7bdabb0691
MerryDev <svgr1808@outlook.de>
2022-04-16 13:22:08 +0200

    Added some backend functions

--- src/main/java/de/digitaldevs/flashblock/FlashBlockPlugin.java
+++ src/main/java/de/digitaldevs/flashblock/FlashBlockPlugin.java
@@ -13,6 +13,7 @@ import de.digitaldevs.gameapi.GameAPI;
 import de.digitaldevs.gameapi.GameAPIPlugin;
 import de.digitaldevs.gameapi.gamestate.GameStateManager;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FlashBlockPlugin extends JavaPlugin {
@@ -29,6 +30,9 @@ public class FlashBlockPlugin extends JavaPlugin {
         this.gameAPI = GameAPIPlugin.getApi();
         this.registerGameStates();
 
+        this.registerCommands();
+        this.registerListener();
+
         Bukkit.getLogger().info(() -> "Plugin was successfully enabled!");
     }
 
@@ -43,5 +47,14 @@ public class FlashBlockPlugin extends JavaPlugin {
         gameStateManager.registerNewGameState(new IngameFightingState(this));
         gameStateManager.registerNewGameState(new EndingState(this));
     }
+
+    private void registerCommands() {
+
+    }
+
+    private void registerListener() {
+        final PluginManager pluginManager = Bukkit.getPluginManager();
+    }
+
 
 }
