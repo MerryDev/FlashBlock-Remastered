bab6615c6c90943f0e19976d2688ddc421dc6b26
waddlespam <max@mlehrke.de>
2022-04-16 12:44:38 +0200

    Initial commit

---
+++ src/main/java/de/digitaldevs/flashblock/gamestate/IngameFightingState.java
@@ -0,0 +1,21 @@
+package de.digitaldevs.flashblock.gamestate;
+
+import de.digitaldevs.flashblock.FlashBlockPlugin;
+import de.digitaldevs.gameapi.gamestate.GameState;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class IngameFightingState implements GameState {
+
+    private final FlashBlockPlugin plugin;
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ src/main/java/de/digitaldevs/flashblock/gamestate/IngameRespawnState.java
@@ -0,0 +1,22 @@
+package de.digitaldevs.flashblock.gamestate;
+
+import de.digitaldevs.flashblock.FlashBlockPlugin;
+import de.digitaldevs.gameapi.gamestate.GameState;
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class IngameRespawnState implements GameState {
+
+    private final FlashBlockPlugin plugin;
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
