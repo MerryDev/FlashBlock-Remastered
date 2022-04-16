cd05416cdd30c97dc57a358d73a4f48e271d58f3
waddlespam <max@mlehrke.de>
2022-04-16 12:11:13 +0200

    Added Var file

---
+++ src/main/java/de/digitaldevs/flashblock/util/Var.java
@@ -0,0 +1,11 @@
+package de.digitaldevs.flashblock.util;
+
+import de.digitaldevs.gameapi.GameAPI;
+
+public class Var {
+
+    public final String prefix = "";
+    public final String noperm = "";
+
+    
+}
