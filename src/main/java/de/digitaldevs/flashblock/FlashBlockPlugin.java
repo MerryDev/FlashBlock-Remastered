a5dd7909bd34bdf5463d02fea00e7e0cbdf815fd
waddlespam <max@mlehrke.de>
2022-04-16 13:07:42 +0200

    Added registerCommands and registerEvents

--- src/main/java/de/digitaldevs/flashblock/FlashBlockPlugin.java
+++ src/main/java/de/digitaldevs/flashblock/FlashBlockPlugin.java
@@ -4,15 +4,21 @@
 
 package de.digitaldevs.flashblock;
 
+import de.digitaldevs.flashblock.commands.LobbysetupCommand;
+import de.digitaldevs.flashblock.commands.SetupCommand;
+import de.digitaldevs.flashblock.commands.StartCommand;
+import de.digitaldevs.flashblock.commands.StatsCommand;
 import de.digitaldevs.flashblock.gamestate.EndingState;
 import de.digitaldevs.flashblock.gamestate.IngameFightingState;
 import de.digitaldevs.flashblock.gamestate.IngameRespawnState;
 import de.digitaldevs.flashblock.gamestate.LobbyState;
+import de.digitaldevs.flashblock.listener.BlockBreakListener;
 import de.digitaldevs.gameapi.GameAPI;
 import de.digitaldevs.gameapi.GameAPIPlugin;
 import de.digitaldevs.gameapi.gamestate.GameStateManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class FlashBlockPlugin extends JavaPlugin {
 
@@ -27,7 +33,8 @@ public class FlashBlockPlugin extends JavaPlugin {
 
         this.gameAPI = GameAPIPlugin.getApi();
         this.registerGameStates();
-
+        this.registerCommands();
+        this.registerEvents();
         Bukkit.getLogger().info(() -> "Plugin was successfully enabled!");
     }
 
@@ -43,4 +50,15 @@ public class FlashBlockPlugin extends JavaPlugin {
         gameStateManager.registerNewGameState(new EndingState(this));
     }
 
+
+    private void registerCommands() {
+        this.getCommand("lobbysetup").setExecutor(new LobbysetupCommand());
+        this.getCommand("setup").setExecutor(new SetupCommand());
+        this.getCommand("start").setExecutor(new StartCommand());
+        this.getCommand("stats").setExecutor(new StatsCommand());
+    }
+
+    private void registerEvents() {
+        getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
+    }
 }
