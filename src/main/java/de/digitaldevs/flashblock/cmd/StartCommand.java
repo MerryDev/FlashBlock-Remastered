3ce243806e17660669c5f9cce8af09d01285b052
waddlespam <max@mlehrke.de>
2022-04-16 12:47:30 +0200

    Initial commit

---
+++ src/main/java/de/digitaldevs/flashblock/cmd/StartCommand.java
@@ -0,0 +1,12 @@
+package de.digitaldevs.flashblock.cmd;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StartCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        return false;
+    }
+}
