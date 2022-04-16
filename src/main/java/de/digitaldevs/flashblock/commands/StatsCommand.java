dff798e0986a3af0238af4f2552d5775b102ab76
waddlespam <max@mlehrke.de>
2022-04-16 12:56:57 +0200

    Initial commit

---
+++ src/main/java/de/digitaldevs/flashblock/commands/StatsCommand.java
@@ -0,0 +1,10 @@
+package de.digitaldevs.flashblock.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class StatsCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) { return false; }
+}
